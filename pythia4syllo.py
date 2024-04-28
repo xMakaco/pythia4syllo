2a2141f7816a7a92c05fa67c46813f3e373d215e
r3lativo <gio.duca13d@gmail.com>
2024-04-29 01:27:57 +0200

    Now possible to choose data to load Added a "syllogism_data_path" argument Changed a bit the style of the chat Improved the file saving and loading Changed max_new_tokens to 75 Changed temperature to 0.5

--- pythia4syllo.py
+++ pythia4syllo.py
@@ -1,7 +1,7 @@
 """
 Use the pythia4syllo to interact with Pythia.
 Usage:
-    pythia4syllo.py [--model_size=<str> --modality=[chat|dataset]]
+    pythia4syllo.py [--model_size=<str> --modality=[chat|dataset] --syllogism_data_path=<str>]
     pythia4syllo.py (-h | --help)
     pythia4syllo.py --version
 
@@ -11,25 +11,35 @@ Options:
     -s --model_size=<str>           The size of pythia to use [default: 70M].
                                     Possible sizes are: "70M", "160M", "410M", "1.0B", "1.4B", "2.8B", "6.9B", "12B".
     -m --modality=[chat|dataset]    chat inputed prompt or prompt to be taken from a dataset [default: chat].
+    -d --syllogism_data_path=<str>  The path to the syllogism_data.tsv.
 """
 
 
 """ Import Libraries """
 from transformers import GPTNeoXForCausalLM, GPTNeoXTokenizerFast  # Pythia via Hugging Face
 import torch  # Needed to work with tensors
-from datetime import datetime  # Date and time, used to create a unique ID for each interaction in the chat
 from docopt import docopt  # Used to use argparser and to manage the version of the program
-from transformers import set_seed  # Ensure reproducibility
+import transformers  # Ensure reproducibility
 import pandas as pd  # Load and create DataFrames to store results
-import os, csv, sys  # Libraries to create the log file
+import os  # Libraries to create the log file
 from tqdm import tqdm  # A simple progress bar
 
 
 
 """ Docopt is used to provide a little documentation for the program """
-args = docopt(__doc__, version = 'Pythia for Syllogisms, ver 0.1')
+args = docopt(__doc__, version = 'Pythia for Syllogisms, ver 0.3')
 model_size = args["--model_size"]   # Default is "70M"
 modality = args["--modality"]       # Default is "chat"
+
+# If modality is dataset, load the syllogisms
+if modality == "dataset":
+    if args["--syllogism_data_path"] is None:
+        raise Exception("Please specify dataset filepath with -d\nSee usage with -h")
+    syllogism_data_path = args["--syllogism_data_path"]
+    # Check that the file exists
+    if not os.path.exists(syllogism_data_path):
+        raise Exception("Can't find the 'syllogism_data.tsv' file")
+    print(f"# {syllogism_data_path} found #")
 
 
 # Pythia comes in 8 sizes, in standard and deduped version (https://github.com/EleutherAI/pythia)
@@ -37,8 +47,7 @@ modality = args["--modality"]       # Default is "chat"
 # If the chosen model size is not valid, print usage and exit
 possible_sizes = ["70M", "160M", "410M", "1.0B", "1.4B", "2.8B", "6.9B", "12B"]
 if model_size not in possible_sizes or modality not in ["chat", "dataset"]:
-    print(docopt(__doc__, "-h"))
-    # exit program
+    raise Exception("modality has to be either 'chat' or 'dataset'")
 
 
 def load_model_and_tokenizer(model_size):
@@ -47,7 +56,10 @@ def load_model_and_tokenizer(model_size):
     input: model_size
     output: model, tokenizer, device
     """
-    set_seed(0)  # Ensure reproducibility
+    transformers.set_seed(0)  # Ensure reproducibility
+    transformers.logging.set_verbosity_error()  # Silence warning
+
+    print(f"# Loading model pythia-{model_size}... #")
 
     # Load the model
     model = GPTNeoXForCausalLM.from_pretrained(
@@ -60,33 +72,32 @@ def load_model_and_tokenizer(model_size):
 
     # Check for CUDA (faster), else run on CPU
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    print(f"\n{5*'#'} YOU ARE USING THE {device.upper()} {5*'#'}\n")
+    print(f"# The model pythia-{model_size} is loaded on the device: {device} #")
     return model, tokenizer, device
 
 
 def ask_question(prompt):
     """
     Interact with the model
-    :param: prompt
-    :type: str
-    :return: answer without prompt
-    :rtype: str
+    input: prompt
+    output: answer (cut, without prompt)
     """
+    # TODO: add input and output types
 
     # If possible, load both the model and the tokenizer to CUDA to speed up the process
-    inputs = tokenizer(prompt, return_tensors = "pt").to(device)
+    inputs = tokenizer(prompt, return_tensors="pt").to(device)
     model.to(device)
 
     # Generate answer with the specified hyperparameters
     tokens = model.generate(
         **inputs,
         #max_length=200,          # SET TO DEFAULT
-        max_new_tokens=25,        #  The number of new tokens, i.e. the lenght of the answer of the model
-        temperature=0.1,          #  Randomness, see https://huggingface.co/blog/how-to-generate#:~:text=A%20trick%20is,look%20as%20follows.
-        top_p=0.6,                #  See https://huggingface.co/blog/how-to-generate#top-p-nucleus-sampling
-        top_k=5,                  #  See https://huggingface.co/blog/how-to-generate#top-k-sampling
-        repetition_penalty=1.0,   #  See https://huggingface.co/docs/transformers/internal/generation_utils#transformers.TFRepetitionPenaltyLogitsProcessor.repetition_penalty
-        do_sample=True,           #  See https://stackoverflow.com/a/71281111/21343868
+        max_new_tokens=75,        # The number of new tokens, i.e. the lenght of the answer of the model
+        temperature=0.5,          # Randomness, see https://huggingface.co/blog/how-to-generate#:~:text=A%20trick%20is,look%20as%20follows.
+        top_p=0.6,                # See https://huggingface.co/blog/how-to-generate#top-p-nucleus-sampling
+        top_k=5,                  # See https://huggingface.co/blog/how-to-generate#top-k-sampling
+        repetition_penalty=1.0,   # See https://huggingface.co/docs/transformers/internal/generation_utils#transformers.TFRepetitionPenaltyLogitsProcessor.repetition_penalty
+        do_sample=True,           # See https://stackoverflow.com/a/71281111/21343868
         #no_repeat_ngram_size=2,
         #num_beams=2,
         eos_token_id=tokenizer.eos_token_id,
@@ -102,130 +113,106 @@ def ask_question(prompt):
     return answer
 
 
-def save_log(text):
-    """
-    This function enables to save the parameters of the interaction in a log file
-    The expected input of "text" is a list of strings
-    Each element of the list will be placed in its column
-    Each column is separated by a tab charachter ('\t', four spaces)
-    """
-    # TODO: add input and output types
-
-    file_path_minilog = f"log_{model_size}_{modality}.tsv"
-
-    # The list of parameters we want to log
-    # The list of parameters we want to log
-    log_components = ["prompt", "response"]
-
-    # If the file does not exist
-    if not os.path.exists(file_path_minilog):
-        # Create the file
-        with open(file_path_minilog, 'w', newline='') as tsvfile:
-            writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
-            # Define the first row of the tsv (its header)
-            writer.writerow([x for x in log_components])
-            print('"log.tsv" not found, created')
-
-        # Append the text in the file.
-        with open(file_path_minilog, 'a', newline='') as tsvfile:
-            writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
-            writer.writerow(text)
-        #print("Log updated")
-
-
 def chat_with_model():
     """
     Interact with the model with manual input
     """
 
-    # Store the time
-    now = datetime.now().strftime("%d%m%Y_%H%M%S")
-    print(f"{5*'#'} GPT-NeoX Chat Interface with Pythia {model_size}. Type 'exit' to stop. {now}  {5*'#'}\n")
-    prompt_num = 1
+    # Prepare the log file
+    chat_log_file = "./chat_log.tsv"
+    chat_log_df = pd.DataFrame(columns=['model_size', 'prompt', 'model_response'])
 
-    # Store the number of the interaction
+    # Enter the chat
+    print(f"# GPT-NeoX Chat Interface with pythia-{model_size}. Type 'exit' to quit #")
     while True:
-        chat_input = input("Input your prompt - ('exit' to quit): ")
-        # 'exit' to quit
-        if chat_input.lower() == 'exit':
-            print("Closing...")
+        chat_input = input("Prompt: ")
+        if chat_input.lower() == 'exit': # 'exit' to quit
+            print("# Your chat log is stored in 'chat_log.tsv' #")
+            print("# Goodbye... #")
             break
+
         # Generate answer
         response = ask_question(chat_input)
         reduced_r = response[len(chat_input):]  # Don't print the chat input again
-        print(f'Model {model_size}, answer to "{chat_input}": \n {reduced_r}')
-        print(f"{3*'#'} End of interaction {prompt_num} {3*'#'}\n")
+        print(f'Model response: {reduced_r}')
 
         # Store answer
-        log = [chat_input, reduced_r]
-        save_log(text=log, mod="short", model_size=model_size, modality=modality)
-        prompt_num += 1
+        current_chat = {
+            'model_size': model_size,
+            'prompt': chat_input,
+            'model_response': reduced_r
+        }
+        
+        # Update the chat_log.tsv
+        new_row_chat = pd.DataFrame([current_chat])  # Combine the new answer in a Dataframe
+        chat_log_df = pd.concat([chat_log_df, new_row_chat], ignore_index=True)  # Concatenate the new answer to the log DataFrame
+        chat_log_df.to_csv(chat_log_file, sep='\t', index=False)  # Create the log.tsv from the log DataFrame
 
 
-def run_model_on_data(syllogism_path, trigger):
+
+def run_model_on_data():
     """
     Load a data.tsv and run the model for each row
     input: path to the syllogism_data.tsv
     output: a log.tsv file
     """
-
-    # Check that the file exists
-    if not os.path.exists(syllogism_path):
-        print("Can't find the 'syllogism_data.tsv' file")
-        sys.exit()
     
     # Load the data into pandas
-    data = pd.read_csv(syllogism_path, delimiter="\t")
+    syllogism_data = pd.read_csv(syllogism_data_path, delimiter="\t")
+    new = True
 
-    # Create an empty DataFrame with these columns
-    log_columns = ['type', 'premise_1', 'premise_2', 'trigger', 'conclusion', 'conclusion_2', 'model_response']
-    log_dataframe = pd.DataFrame(columns=log_columns)
+    # Load the log file if it exists
+    if os.path.exists(f"log_{model_size}_{modality}.tsv"):
+        data_log_df = pd.read_csv(f"log_{model_size}_{modality}.tsv", delimiter='\t')
+        new = False
+    else:
+        # Create the file where to store log if it doesn't exist
+        data_log_columns = ['type', 'context', 'premise_1', 'premise_2', 'trigger', 'conclusion', 'conclusion_2', 'model_response']
+        data_log_df = pd.DataFrame(columns=data_log_columns)
+        
+    # Ask for context and trigger
+    context = input("(Optional) Context: ")
+    trigger = input("Trigger: ")
 
     # For each row in the syllogism file
     # tqdm is used to have a progress bar
-    for index, row in tqdm(data.iterrows(), total=data.shape[0]):
+    print("# Generating answers... #")
+    for index, row in tqdm(syllogism_data.iterrows(), total=syllogism_data.shape[0]):
         type = row.type
-        p1 = row.premise_1
-        p2 = row.premise_2
-        c1 = row.conclusion
-        c2 = row.conclusion_2
+        p1 = row.premise_1;     p2 = row.premise_2
+        c1 = row.conclusion;    c2 = row.conclusion_2
 
-        full = p1 + p2 + trigger  # Combine the full prompt
+        # Combine the full prompt
+        full = context + p1 + p2 + trigger  
         model_response = ask_question(full)
         model_response = model_response[len(full):]  # Cut the answer of the model (remove the prompt)
-        #print(full + model_response)
 
         # Assign each variable to the right column to create the final log
         new_result = {
             'type': type,
-            'premise_1': p1,
-            'premise_2': p2,
+            'context': context,
+            'premise_1': p1, 'premise_2': p2,
             'trigger' : trigger,
-            'conclusion': c1,
-            'conclusion_2': c2,
+            'conclusion': c1, 'conclusion_2': c2,
             'model_response': model_response
         }
 
         # Combine the new answer in a Dataframe
         new_row = pd.DataFrame([new_result])
         # Concatenate the new answer to the log DataFrame
-        log_dataframe = pd.concat([log_dataframe, new_row], ignore_index=True)
+        data_log_df = pd.concat([data_log_df, new_row], ignore_index=True)
     
-    # Create the log.tsv from the log DataFrame
-    file_name = f"log_{model_size}_{modality}.tsv"
-    log_dataframe.to_csv(file_name, sep='\t', index=False)
-    print("Done!")
-
+    data_log_df.to_csv(f"log_{model_size}_{modality}.tsv", sep='\t', index=False)
+    if new == True:
+        print(f"# 'log_{model_size}_{modality}.tsv' created! #")
+    else:
+        print(f"# 'log_{model_size}_{modality}.tsv' updated! #")
+    return
 
 
 
 """ HERE THE MODEL IS ACTUALLY LOADED AND THE CHOSEN MODALITY RUN """
-
 model, tokenizer, device = load_model_and_tokenizer(model_size)
-syllogism_path = "./syllogism_data.tsv"
-
-if modality == "chat":  # Default
-    chat_with_model()
-elif modality == "dataset":
-    run_model_on_data(syllogism_path, trigger="It follows that: ")
+if modality == "chat": chat_with_model()
+elif modality == "dataset": run_model_on_data()
     
